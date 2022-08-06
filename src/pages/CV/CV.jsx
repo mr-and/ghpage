538a90f6949673d9d186102c769405e0e9d8dbdc
av.bobrov <av.bobrov@bftcom.com>
2022-08-07 00:11:03 +0300

    add cv

--- src/pages/CV/CV.jsx
+++ src/pages/CV/CV.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
+import resume from "../../assets/blank_cv.pdf";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+
+import "./CV.css";
 
 function CV() {
-  return <div>CV</div>;
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div className="resumeBox">
+      <div class="resumeContainer">
+        <Document file={resume} className="resume">
+          <Page pageNumber={1} scale={width > 786 ? 1.5 : 0.6} />
+        </Document>
+        <button
+          href={resume}
+          target="_blank"
+        >
+          <a href={resume} download>Download</a>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default CV;
